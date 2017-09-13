ed02f382d3225a66acaf7fe62aa10e0c0c81011e
Alina Heghedus <alinaheghedus@yahoo.com>
2017-09-13 15:24:56 -0400

    complete lazyloading of images

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -3,9 +3,9 @@ webpack = require('webpack');
 
 gulp.task('scripts', function(callback) {
 	webpack(require('../../webpack.config.js'), function(err, stats) {
-		if(err) {
+		if (err) {
 			console.log(err.toString());
-		}
+		};
 
 		console.log(stats.toString());
 		callback();
